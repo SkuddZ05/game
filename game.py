2ffa246d78996327a6f62c60de72b0fab16222d3
SkuddZ05 <ollie.skudder@sbhsstudent.school.nz>
2026-08-12 15:09:46 +1200

    greeting

---
+++ game.py
@@ -0,0 +1,8 @@
+#greeting the user
+print("Hello")
+
+
+#ask user for name, save as variable
+name = input("What is your name? ").capitalize()
+if name == "Ollie" or "Oli":
+    print("Yes! Me too!!")
