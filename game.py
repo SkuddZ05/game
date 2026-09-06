2fde33de1eebd62bc9c783edd33e84e4390c5b2c
SkuddZ05 <ollie.skudder@sbhsstudent.school.nz>
2026-09-07 09:45:52 +1200

    tested had indentation errors

--- game.py
+++ game.py
@@ -1,8 +1,28 @@
+import pygame
+WIDTH, HEIGHT, FPS = 800, 600, 60
+NAVY = (9, 24, 43)
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Star Catcher")
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill(NAVY)
+        pygame.display.flip()
+    pygame.quit()
+if __name__ == "__main__":
+ run_game()
+
 #greeting the user
-print("Hello")
+#print("Hello")
 
 
 #ask user for name, save as variable
-name = input("What is your name? ").capitalize()
-if name == "Ollie" or "Oli":
-    print("Yes! Me too!!")
+#name = input("What is your name? ").capitalize()
+#if name == "Ollie" or "Oli":
+    #print("Yes! Me too!!")
