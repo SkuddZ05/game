8380cc9726494601828e1e32a34cb5fb77de5a0e
SkuddZ05 <ollie.skudder@sbhsstudent.school.nz>
2026-09-11 10:49:39 +1200

    declaring variables and assigning values

--- game.py
+++ game.py
@@ -1,6 +1,17 @@
 import pygame
-WIDTH, HEIGHT, FPS = 800, 600, 60
+import sys
+WIDTH, HEIGHT, FPS = 1945, 1025, 95
 BLACK= (0, 0, 0)
+
+BG_COLOR = (20, 20, 30)        # Dark blue/grey
+BAR_BG_COLOR = (45, 45, 60)    # Empty bar color
+BAR_FILL_COLOR = (0, 255, 150) # Bright green progress color
+TEXT_COLOR = (255, 255, 255)
+
+font = pygame.font.SysFont("Arial", 30)
+
+
+
 def run_game():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -16,5 +27,5 @@ def run_game():
         pygame.display.flip()
     pygame.quit()
 if __name__ == "__main__":
- run_game()
+    run_game()
  # Loading Screen
